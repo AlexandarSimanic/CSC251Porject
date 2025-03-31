749fe74bffe26e12daec622e67d259b68b3b3085
AlexandarSimanic <58239043+AlexandarSimanic@users.noreply.github.com>
2025-03-31 13:38:41 -0400

    Add files via upload

---
+++ Policy.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Policy
+{
+   // policy number
+   // provider name
+   // policyholder's first name and last name
+   // policyholder's age
+   // policyholder's smoking status
+   // policyholder's height
+   // policyholder's weight
+   
+   // BMI = (policyholder's weight * 703) / (policyholder's height^2)
+   
+   // insurance policy has a base fee of $600
+   // if the policyholder is over 50, add $75
+   // if the policyholder is a smoker, add $100
+   // if the policyholder has a BMI over 35, add a fee that is (BMI - 35) * 20
+   
+   
+   // Please enter the Policy Number: 
+   // Please enter the Provider Name: 
+   // Please enter the Policyholder's First Name: 
+   // Please enter the Policyholder's Last Name: 
+   // Please enter the Policyholder's Age: 
+   // Please enter the Policyholder's Smoking Status (smoker/non-smoker): 
+   // Please enter the Policyholder's Height (in inches): 
+   // Please enter the Policyholder's Weight (in pounds): 
+   
+   // Policyholder's BMI: 
+   // Policy Price: 
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+}
